a0fac4eae14e9c246e9b0323a57ae797ba0b2243
Belal Elkhateeb <belalelmansy796@gmail.com>
2025-02-08 22:39:53 +0200

    Linq part 3

--- ThePertendCompanyApplication/Program.cs
+++ ThePertendCompanyApplication/Program.cs
@@ -4,8 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
-
+using System.Collections;
 namespace ThePertendCompanyApplication
 {
     internal class Program
@@ -23,15 +22,34 @@ namespace ThePertendCompanyApplication
             }
 
             List<Department> departments = Data.GetDepartMent();
+////First Case of Where >>>>>>>>Filterations
             var filteration = departments.Where(depart => depart.ShortName == "HR" || depart.ShortName == "TE");
+            //second Case of Where >>>>>>>>
+
+            filteration = departments.Where((p, i) => p.ShortName[0]=='H'&&i <5/*first Five Element only */);//You can't Do this With Query syntax
+
             foreach (var depart in filteration)
             {
                 Console.WriteLine($"ID :{depart.Id}");
                 Console.WriteLine($"Shortname :{depart.ShortName}");
                 Console.WriteLine($"LongName :{depart.LongName}\n");
-
             }
-
+            //Case one of Select
+////Select:Transfotmaion operator If i Want to select All My Data(FirstName,LastName...... ) 
+////Projrct /Transform every Element in Seq To A new Element in Seq of new DataType
+            var NameVal = employees.Select(select=> new { ID=select.FirstName , select.LastName}); //Anonymous Type 
+            foreach (var item in NameVal)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(" ");
+            //Case Two of Select >>Indexed Select Valid only in fluent syntax Query
+            List<Products> products = Data.GetProducts();
+            var Result =products.Select((sel,i) => new { index=i, Category = sel.Category, IsStocked = sel.IsStocked });
+            foreach (var items in Result)
+            {
+                Console.WriteLine(items);
+            }
 
             Console.ReadLine();
         }
